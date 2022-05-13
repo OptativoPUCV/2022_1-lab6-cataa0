e583acbb008c9c6e20081ec455d8f6294fc3897f
cataa0 <>
2022-05-13 15:20:28 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -44,14 +44,49 @@ void print_node(Node* n){
 }
 
 int is_valid(Node* n){
-
+  int i, j;
+  for(i=0;i<9;i++)
+    for(j=0;j<9;j++)
+    {
+        scanf("%c",&c);
+        a[i][j]=c-'0';
     return 1;
+  for(i=0;i<9;i++){
+    flag=0x0000;
+    for(j=0;j<9;j++)
+        flag|=1<<(a[i][j]-1);
+    if(flag!=0x01FF)
+        reporta("fila",i,j-1);
+  }  
+  for(j=0;j<9;j++){
+    flag=0x0000;
+    for(i=0;i<9;i++)
+        flag|=1<<(a[i][j]-1);
+    if(flag!=0x01FF)
+    reporta("col",i-1,j);
+  }
+  for(si=0;si<3;si++){
+    for(sj=0;sj<3;sj++)
+    {
+        flag=0x0000;
+        for(i=0;i<3;i++)
+        {
+            for(j=0;j<3;j++)
+                flag|=1<<(a[si*3+i][sj*3+j]-1);
+
+        }
+        if(flag!=0x01FF)
+            reporta("bloque",si*3+i-1,sj*3+j-1);
+    }
+  }
+    
 }
 
 
 List* get_adj_nodes(Node* n){
-    List* list=createList();
-    return list;
+  List* list=createList();
+  
+  return list;
 }
 
 
