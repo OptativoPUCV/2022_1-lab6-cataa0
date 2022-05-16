a739924dcd8953b63e12ea83dbe29ebc81faa6cf
cataa0 <>
2022-05-16 01:37:24 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -72,7 +72,7 @@ int is_valid(Node* n){
     for(j = 0 ; j < 9 ; j++){
       auxi=3*(i/3)+(j/3);
       auxj=3*(i%3)+(j%3);  
-      if(n->sudo[auxi][auxj] != 0){
+      if([aux[n->sudo[auxi][auxj]] != 0){
         return 0;
         if(aux[n->sudo[auxi][auxj]] == 0 && aux[n->sudo[auxi][auxj]] != 0){
           aux[n->sudo[auxi][auxj]] = 1;
